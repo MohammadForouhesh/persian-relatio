fbb93fc5d4d2422d5c224802663de15816e3df79
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-02-26 10:55:11 +0330

    Update datasets.py

---
+++ src/datasets.py
@@ -0,0 +1,45 @@
+from ast import literal_eval
+from io import StringIO
+
+import pandas as pd
+import requests
+
+
+def list_datasets():
+    return """
+    List of available datasets:
+    Trump Tweet Archive
+    - function call: load_trump_data()
+    - format: 'raw', 'split_sentences', 'srl_res'
+    - allennlp version: 0.9
+    - srl model: srl-model-2018.05.25.tar.gz
+    """
+
+
+def load_trump_data(format: str):
+    """
+    Load processed tweets from the Trump Tweet Archives (https://www.thetrumparchive.com/).
+    :param format: either 'raw' (i.e. dataframe with raw text), 'split_sentences' (i.e. result from split_sentences())
+                   or 'srl_res' (i.e. result from run_srl())
+    :return: The desired object.
+    """
+
+    if format == "raw":
+        r = requests.get(
+            "https://www.dropbox.com/s/lxqz454n29iqktn/trump_archive.csv?dl=1"
+        )
+        r = pd.read_csv(StringIO(r.text))
+    elif format == "split_sentences":
+        r = requests.get(
+            "https://www.dropbox.com/s/coh4ergyrjeolen/split_sentences.json?dl=1"
+        )
+        r = literal_eval(r.text)
+    elif format == "srl_res":
+        r = requests.get("https://www.dropbox.com/s/54lloy84ka8mycp/srl_res.json?dl=1")
+        r = literal_eval(r.text)
+    else:
+        raise ValueError(
+            "file argument should either be raw, split_sentences or srl_res"
+        )
+
+    return r
