b5688c7173d9f73cb94cfe314dfaae43c6edad3f
Mohammad Forouhesh <Mohammad.Forouhesh@Outlook.com>
2022-03-01 19:54:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
 # df = load_trump_data("raw")
-df = pd.read_excel('/content/drive/MyDrive/Metodata/clf_status_id.xlsx').sample(100000)
+df = pd.read_excel('clf_status_id.xlsx').sample(100000)
 print(df.columns)
 df = df[['status_id', 'text']]
 df = df.rename(columns={'status_id': 'id', 'text': 'doc'})
 print(df.head())
+
+tqdm.pandas()
+
+
+df.replace('', float('NaN'), inplace=True)
+df.replace(' ', float('NaN'), inplace=True)
+df.doc = df.doc.progress_apply(lambda item: remove_redundant_characters(remove_emoji(item)))
+df.dropna(inplace=True)
