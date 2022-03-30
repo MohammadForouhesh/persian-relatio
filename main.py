e9845814bfe2f9bacb9abae4ad8ba11658a3335b
keif-forouhesh <mohammadh.forouhesh@gmail.com>
2022-03-30 06:09:21 -0400

    #3 pull request.

--- main.py
+++ main.py
@@ -52,14 +52,12 @@ srl_res = run_srl(
 file = open('persian.txt', 'r')
 spacy_stopwords = list(file.read().splitlines())
 
-# NB: This step usually takes several minutes to run. You might want to grab a coffee.
-
 narrative_model = build_narrative_model(
     srl_res=srl_res,
     sentences=split_sentences[1],
     embeddings_type="gensim_full_model",  # see documentation for a list of supported types
     embeddings_path='w2v_emb.bin',
-    n_clusters=[[1000]],
+    n_clusters=[[100]],
     top_n_entities=1000,
     # stop_words=spacy_stopwords,
     remove_n_letter_words=1,
@@ -168,9 +166,7 @@ temp = complete_narratives[["ARG0_lowdim", "ARG1_lowdim", "B-V_lowdim"]]
 temp.columns = ["ARG0", "ARG1", "B-V"]
 temp = temp[(temp["ARG0"] != "") & (temp["ARG1"] != "") & (temp["B-V"] != "")]
 temp = temp.groupby(["ARG0", "ARG1", "B-V"]).size().reset_index(name="weight")
-temp = temp.sort_values(by="weight", ascending=False)  # .iloc[
-#     0:100
-# ]  # pick top 100 most frequent narratives
+temp = temp.sort_values(by="weight", ascending=False).iloc[0:100]  # pick top 100 most frequent narratives
 temp = temp.to_dict(orient="records")
 
 for l in temp:
