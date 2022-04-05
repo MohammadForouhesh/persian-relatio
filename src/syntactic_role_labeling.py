d8520888b7ed9d3ef5d7b70afd30e606bbc98621
mforouhesh <17898264+MohammadForouhesh@users.noreply.github.com>
2022-04-05 19:29:28 +0430

    Update syntactic_role_labeling.py

--- src/syntactic_role_labeling.py
+++ src/syntactic_role_labeling.py
@@ -29,7 +29,7 @@ class SyntacticDP():
 
 def extract_pos_words(pos_tagged: List[Tuple[str, str]], role: str) -> Generator[str, None, None]:
     for item in pos_tagged:
-        if item[1] == role: yield item[0]
+        if item[0] == role: yield item[1]
 
 
 def find_pos_index(pos_tagged: List[Tuple[str, str]], role: str) -> int:
@@ -43,15 +43,15 @@ def find_pos_word(pos_tagged: List[Tuple[str, str]], role: str, index: int) -> s
 
 
 def pos2srl(pos_tagged: List[Tuple[str, str]]) -> Generator[Tuple[str, str], None, None]:
-    keys = {'PRO': 'ARG0', 'V': 'V', 'N': 'ARG1', 'DET': 'ARG1', 'ADJ': 'ARG2',
-            'NUM': 'ARGM-TMP', 'CON': 'ARGM-TMP', 'ADV': 'ARG2'}
+    keys = {'PRO': 'ARG0', 'V': 'V', 'N': 'ARG1', 'ADJ': 'ARG2',
+            'NUM': '', 'CON': '', 'ADV': ''}
     verb_ind = find_pos_index(pos_tagged, 'V')
     for ind, item in enumerate(pos_tagged):
         if item[1] == 'N' and ind < verb_ind-3:     yield item[0], 'ARG0'
         elif item[1] == 'V' and ind > verb_ind+1:   yield item[0], 'ARG1'
         else:
             try:    yield item[0], keys[item[1]]
-            except: yield item[0], 'ARGM-TMP'
+            except: yield item[0], ''
 
 
 def concat_srl(rel_tagged: Generator[Tuple[str, str], None, None]) -> Generator[Tuple[str, str], None, None]:
@@ -68,32 +68,37 @@ def concat_srl(rel_tagged: Generator[Tuple[str, str], None, None]) -> Generator[
         ind = j
 
 
-def mock_description(rel_tagged: Generator[Tuple[str, str], None, None]) -> str:
-    return ' '.join([f"[{': '.join(item[::-1])}]" for item in rel_tagged])
+def mock_description(rel_tagged: List[Tuple[str, str]]) -> str:
+    return ' '.join([f"[{': '.join(item[::-1])}]" if item[0] != '' else item[1] for item in rel_tagged])
 
 
-def mock_tags(rel_tagged: Generator[Tuple[str, str], None, None]) -> Generator[str, None, None]:
+def mock_tags(rel_tagged: List[Tuple[str, str]]) -> Generator[str, None, None]:
     for item in rel_tagged:
         yield f'B-{item[0]}'
         for _ in item[1].split()[1:]:
-            yield f'I-{item[0]}'
+            if item[0] != 'V':  yield f'I-{item[0]}'
+            else:               yield f'B-{item[0]}'
 
 
 def sdp2srl_mock(pos_tagged: List[Tuple[str, str]]) -> List[Dict[str, Union[str, List[str]]]]:
-    return [{'verbs': [{'verb': find_pos_word(pos_tagged, 'V', 0),
-                        'description': mock_description(concat_srl(pos2srl(pos_tagged))),
-                        'tags': list(mock_tags(concat_srl(pos2srl(pos_tagged))))}],
+    concat_srl_var = list(concat_srl(pos2srl(pos_tagged)))
+    return [{'verbs': [{'verb': find_pos_word(concat_srl_var, 'V', 0),
+                        'description': mock_description(concat_srl_var),
+                        'tags': list(mock_tags(concat_srl_var))}],
              'words': [item[0] for item in pos_tagged]}]
 
 
 if __name__ == '__main__':
     sdp = SyntacticDP()
-    pos = sdp(['به خواست خدا این فریمورک کار میکند'])
+    tagger = WapitiPosTagger()
+    print(tagger['به خدا قسم میخورم این فریمرک کار میکند'])
+    pos = sdp(['شروع کار کلاب صیانت از ۴ میاشد'])
     print(pos)
     print(sdp(['من به مدرسه میروم', 'به گزارش افق پیشبینی حاکی از افت سهام نیویورک است']))
     pos = sdp(['به گزارش افق پیشبینی حاکی از افت سهام نیویورک است'])
     print(pos)
 
-    real_world_test_case = "شکست خوردن هفتمین دور مذاکرات وین پس از توقف شش ماهه"
+    real_world_test_case = "اسب تازی بخوبی از پس همه‌ تست ها بر می اید"
+    print(tagger[real_world_test_case])
     pos = sdp([real_world_test_case])
     print(pos)
